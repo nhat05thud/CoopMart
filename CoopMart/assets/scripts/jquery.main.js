141a1010477b4eb6a999e3bfc6990f1f523131b8
NhatNguyen <nhat.nguyend@s3corp.com.vn>
2020-11-20 16:32:14 +0700

    update

--- CoopMart/assets/scripts/jquery.main.js
+++ CoopMart/assets/scripts/jquery.main.js
@@ -12,6 +12,28 @@ function myfunload() {
         autoplay: true,
         autoplaytimeout: 5000
     });
+    $(".product-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        navText: [
+            '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>',
+            '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>'
+        ],
+        autoplay: false,
+        autoplaytimeout: 5000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            992: {
+                items: 5
+            }
+        }
+    });
 }
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
